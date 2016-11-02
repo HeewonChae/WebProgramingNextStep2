e016df8e2e6ee9adca51b3507cd8151e573f5132
HeewonChae <korman2444@gmail.com>
2016-11-03 06:48:36 +0900

    no message

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,4 +1,6 @@
 String.prototype.format = function() {
+	$(".answerWrite input[type=submit]").click(addAnswer);
+	
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
     return typeof args[number] != 'undefined'
@@ -8,3 +10,22 @@ String.prototype.format = function() {
   });
 };
 
+function addAnswer(e){
+	e.preventDefault();
+	var queryString = $("form[name=answer]").serialize();
+	
+	$.ajax({
+		type : 'post' ,
+		url : '/api/qna/addAnswer' ,
+		data : queryString,
+		dataType : 'json' ,
+		error : onError,
+		success : onSuccess,
+	});
+}
+
+function onSuccess(json, status){
+	var answerTeplate = $("#answerTemplate").html();
+	var template = answerTemplate.format(json.writer, new Date(json, createdDate), json.contents, json.answerId);
+	$(".qna-comment-slipp-articles").prepend(template);
+}
